5477ec5ab13bdb23962743a348a22f054191b3c6
swarupcoding56 <swarupmtra3456@gmail.com>
2023-11-12 16:55:28 +0530

    summation of two digits

---
+++ additionoftwointeger.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;int m;
+    printf("enter the first numeber");
+    scanf("%d",&n);
+    printf("enter the second numeber");
+    scanf("%d",&m);
+    printf("the summation of %d and %d  is %d",n,m,n+m);
+    return 0;
+}
