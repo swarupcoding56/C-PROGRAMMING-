712bf641d4ceed6d6f825b0d182c6611a1677453
swarupcoding56 <swarupmtra3456@gmail.com>
2023-11-12 17:12:29 +0530

    fractional part

---
+++ frractional part of any decimal number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float x;
+    printf (" enter any decimal number :");
+    scanf (" %f",&x);
+     int y=x;
+     float z=( x-y);
+    printf( " the fractioanl part  of  the number =%f",z);
+    return 0;
+}
