492abb1fccb2556040112db799067e18451b9954
swarupcoding56 <swarupmtra3456@gmail.com>
2023-11-12 17:06:02 +0530

    multipication and division

---
+++ multipicationanddivision.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;int m;
+    printf("enter the first numeber");
+    scanf("%d",&n);
+    printf("enter the second numeber");
+    scanf("%d",&m);
+    printf("the multipication of %d and %d  is %d\n the division of %d and %d is %d",n,m,n*m,n,m,n/m);
+    return 0;
+}
