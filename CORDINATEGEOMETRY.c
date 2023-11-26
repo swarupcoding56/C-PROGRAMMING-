05a3dfa94fcd6c67689bea18d131823fd71cda28
swarupcoding56 <swarupmtra3456@gmail.com>
2023-11-27 01:24:03 +0530

    cordinate geometry

--- CORDINATEGEOMETRY.c
+++ CORDINATEGEOMETRY.c
@@ -2,18 +2,10 @@
 int main (){
 int  x,y;
 printf( " enter the cordinates:");
-scanf("%d%d",&x,&y);
-if ( x ==0 && y==0 ){
-printf( " the cordinate is on origin ");
-}
-else if ( x ==0){
-    printf ( "the cordinate is on x axis  ");
-}
-else if ( y==0){ 
-    printf ( "the coridnate is on y axis");
-}
-else {
-    printf (" the  cordinate is not in the x axis also not in y axis and  not on  the origin");
-}
+scanf("%d,%d",&x,&y);
+if ( x ==0 && y==0 )printf( " the cordinate is on origin ");
+else if ( x ==0 && y!=0 && y>0)printf ( "the cordinate is on y axis  ");
+else if ( y==0 && x!=0 && x>0)printf ( "the coridnate is on x axis");
+else printf (" the  cordinate is not in the x axis also not in y axis and  not on  the origin");
 return 0; 
 }
