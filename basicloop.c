40d2d3dbd51fe2dc78afe75c8ccc877f22898be0
swarupcoding56 <swarupmtra3456@gmail.com>
2023-11-27 22:51:43 +0530

    loop

---
+++ basicloop.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the numeber you want : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        printf("i love you\n ");
+    }
+    return 0;
+}
